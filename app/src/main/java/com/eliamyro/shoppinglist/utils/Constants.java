8784c6a919cf7ace774095be09fd4443e4b21952
Elias Myronidis <elias.myronidis@hotmail.com>
2016-05-21 12:56:32 +0300

    • updated dependencies in gradle file. • added values for different screen types. • added layouts.

---
+++ app/src/main/java/com/eliamyro/shoppinglist/utils/Constants.java
@@ -0,0 +1,33 @@
+package com.eliamyro.shoppinglist.utils;
+
+/**
+ * Created by Elias Myronidis on 21/5/16.
+ */
+
+import com.eliamyro.shoppinglist.BuildConfig;
+
+/**
+ * Constants class store most important strings and paths of the app
+ */
+public final class Constants {
+
+    /**
+     * Constants related to locations in Firebase, such as the name of the node
+     * where active lists are stored (ie "activeLists")
+     */
+
+
+    /**
+     * Constants for Firebase object properties
+     */
+    public static final String FIREBASE_PROPERTY_LISTNAME = "listName";
+
+    /**
+     * Constants for Firebase URL
+     */
+    public static final String FIREBASE_URL = BuildConfig.UNIQUE_FIREBASE_ROOT_URL;
+
+    /**
+     * Constants for bundles, extras and shared preferences keys
+     */
+}
